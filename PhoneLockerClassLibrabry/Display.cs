03295d6337b0eb580f15693d3fa84f31544ec7bb
Marc M <201810695@post.au.dk>
2021-03-17 09:54:17 +0100

    Added Display and logging with interfaces

---
+++ PhoneLockerClassLibrabry/Display.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PhoneLocker;
+
+namespace PhoneLockerClassLibrary
+{
+    public class Display : IDisplay
+    {
+        public void DisplayText(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
