acb585453652fb91a877fe233cca8b892f1b5f74
Sabrowna <60734481+Sabrowna@users.noreply.github.com>
2021-03-17 09:58:04 +0100

    Impl. methods in the interfaces IDoor, IStationControl and Event for Door

--- PhoneLockerClassLibrabry/IStationControl.cs
+++ PhoneLockerClassLibrabry/IStationControl.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PhoneLockerClassLibrary;
 
 namespace PhoneLocker
 {
     public interface IStationControl
     {
+        public Enum PhoneLockerState();
+        public event EventHandler<DoorOpenedEventArgs> DoorOpenedEvent;
+        //public event EventHandler<RFIDDetectedEventArgs> RfidDetectedEvent;
+        public IDoor Door { get; set; }
+        public IRFIDReader RfidReader { get; set; }
+
+        public bool DoorOpened();
+        public bool RFIDDetected(int ID);
+        public bool CheckId(int OldId, int Id);
+        //HandleDoorOpenedEvent
+
+        //Skal denne metode impl. her jævnfør Marc/Mads, eller i IDoor, jævnfør vores diagram?
+        //protected virtual void OnDoorOpened(DoorOpenedEventArgs e)
+        //{
+        //    //DoorOpenedEvent
+        //}
+
+
     }
+
+    
 }
