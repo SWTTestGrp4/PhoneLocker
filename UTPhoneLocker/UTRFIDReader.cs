3f1539646050690ae76396e3df11aaca16aa016a
Jesper Trøiborg Laursen <au622885@uni.au.dk>
2021-03-25 13:50:06 +0100

    Started UT of FIDReader

--- UTPhoneLocker/UTRFIDReader.cs
+++ UTPhoneLocker/UTRFIDReader.cs
@@ -1,10 +1,25 @@
 ﻿using NUnit.Framework;
+using PhoneLocker;
+using PhoneLockerClassLibrary;
 
 namespace UsbSimulator.Test
 {
     [TestFixture]
     public class UTRFIDReader
     {
-        
+        private IRFIDReader _uut;
+        private RFIDDetectedEventArgs _receivedArgs;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new RFIDReader();
+        }
+
+        [Test]
+        public void OnRFIDDetected_ReceiveID12_InvokeEventWithValue12()
+        {
+            _uut.RFIDDetectedEvent += (o, args) => { _receivedArgs = args; };
+        }
     }
 }
