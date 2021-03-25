0820659ad5ba9be7169c7adff3a756f20727c624
Sabrowna <60734481+Sabrowna@users.noreply.github.com>
2021-03-25 23:29:31 +0100

    FakeStationControl and Substitute.For<> deleted

--- UTPhoneLocker/UTDoor.cs
+++ UTPhoneLocker/UTDoor.cs
@@ -25,10 +25,6 @@ namespace UsbSimulator.Test
                     _receivedEventArgs = args;
                 };
 
-            _sourceDoor = new Door();
-            _uutControl = new FakeStationControl(_sourceDoor);
-
-
         }
 
         [Test]
@@ -72,60 +68,6 @@ namespace UsbSimulator.Test
             _uut.DoorLocked = true;
             bool newVal = _uut.DoorLocked;
             Assert.That(_uut.DoorLocked, Is.EqualTo(newVal));
-        }
-        /*
-        [Test]
-        public void OnDoorOpened_DoorHandlerTest_TestSuccess()
-        {
-            _receivedEventArgs = new DoorLockedEventArgs() {DoorLocked = true };
-            _uut.OnDoorOpened(_receivedEventArgs);
-            Assert.That(_uut.DoorLocked, Is.EqualTo(true));
-
-        }
-        */
-        /*
-        [TestCase(true)]
-        public void StationControlDoorEvent_DifferentArguments_DoorBoolIsTrue(bool locked)
-        {
-            DoorLockedEventArgs doorLockedEvent = new DoorLockedEventArgs() {DoorLocked = true};
-            //This class tests the stationsControl by using Nsubstitute. It injects the source into the receiver to make sure that the event gets triggered on the right place
-            _sourceDoor.DoorLockedEvent += Raise.EventWith(doorLockedEvent);
-            _sourceDoor.OnDoorOpened(doorLockedEvent);
-            Assert.That(_uutControl.DoorLocked, Is.EqualTo(locked));
-        }
-        
-        [TestCase(false)]
-        public void StationControlDoorEvent_DifferentArguments_DoorBoolIsFalse(bool locked)
-        {
-            //This class tests the stationsControl by using Nsubstitute. It injects the source into the receiver to make sure that the event gets triggered on the right place
-            _sourceDoor.DoorLockedEvent += Raise.EventWith(new DoorLockedEventArgs() { DoorLocked = false });
-            Assert.That(_uutControl.DoorLocked, Is.EqualTo(locked));
-        }
-        */
-    }
-
-    public class FakeStationControl: IStationControl
-    {
-        public bool DoorLocked { get; set; }
-        public int _oldId { get; set; }
-        public int Rfid { get; set; }
-        public DoorLockedEventArgs DoorEventArgs { get; set; }
-
-        public void RfidDetected()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IDoor _door { get; set; }
-        public PhoneLockerState _state { get; set; }
-        public IChargeControl _charger { get; set; }
-        public ILogging _logging { get; set; }
-        public IDisplay _display { get; set; }
-        public IRFIDReader _rfidReader { get; set; }
-
-        public FakeStationControl(IDoor door)
-        {
-            _door = door;
         }
     }
 }
