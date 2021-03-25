09bcd5bdba9a5f4606a0e74ec95529ce5f35ec4e
Jesper Trøiborg Laursen <au622885@uni.au.dk>
2021-03-25 16:02:48 +0100

    Fixed Namespaces

--- PhoneLockerClassLibrabry/StationControl.cs
+++ PhoneLockerClassLibrabry/StationControl.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 using PhoneLocker;
-using PhoneLockerClassLibrary;
 
-namespace PhoneLocker
+namespace PhoneLockerClassLibrary
 {
     public class StationControl: IStationControl
     {
