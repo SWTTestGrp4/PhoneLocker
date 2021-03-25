07650dbd416628b7abffc7ebf12b1fbfd5c0a6c0
Jesper Trøiborg Laursen <au622885@uni.au.dk>
2021-03-25 20:26:50 +0100

    Maybe bug fix...

--- UTPhoneLocker/UTLogFileDAL.cs
+++ UTPhoneLocker/UTLogFileDAL.cs
@@ -15,7 +15,7 @@ namespace UsbSimulator.Test
         [SetUp]
         public void Setup()
         {
-            path = "C:/Users/jespe/source/repos/PhoneLocker/UTPhoneLocker/bin/Debug/netcoreapp3.1/PhoneLockerLog.txt";
+            path = "PhoneLockerLog.txt";
             File.Create(path).Close();
             _uut = new LogFileDAL();
 
